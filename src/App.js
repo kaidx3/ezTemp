d26b7d6e47f58ad13d18c7743f6c8f54151fc26d
Kaiden Flaig <kaidenflaig335@gmail.com>
2023-02-02 14:19:17 -0700

    completes the initial styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, {useState} from 'react'
 import axios from 'axios';
 import Geocode from "react-geocode";
+import cloudyImage from "./assets/wi-cloudy.svg";
+import sunnyImage from "./assets/wi-day-sunny.svg"
+import rainyImage from "./assets/wi-showers.svg"
+import snowyImage from "./assets/wi-snow-wind.svg"
 
 function App() {
   const [data, setData] = useState({})
+  const [weatherImage, setWeatherImage] = useState('')
   let latitude
   let longitude
   const [location, setLocation] = useState('')
@@ -28,9 +33,28 @@ function App() {
   const getCityDataFromCoords = () => {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=2f7da05d2c6127b84619fafd58094128`).then((response) => {
       setData(response.data)
-      console.log(response.data)
+      updateWeatherImage(response.data)
     })
     setLocation('')
+  }
+
+  const updateWeatherImage = (data) => {
+    if (data.weather[0].main == "Clouds" || data.weather[0].main == "Mist"){
+      setWeatherImage(cloudyImage)
+      document.body.style.backgroundColor = "grey";
+    }
+    if (data.weather[0].main == "Clear"){
+      setWeatherImage(sunnyImage)
+      document.body.style.backgroundColor = "orange";
+    }
+    if (data.weather[0].main == "Rain"){
+      setWeatherImage(rainyImage)
+      document.body.style.backgroundColor = "#53789e";
+    }
+    if (data.weather[0].main == "Snow"){
+      setWeatherImage(snowyImage)
+      document.body.style.backgroundColor = "#C0F6FB";
+    }
   }
 
   return (
@@ -39,26 +63,29 @@ function App() {
         <input value={location} onChange={event => setLocation(event.target.value)} type="text" placeholder='Enter Location' onKeyPress={searchLocation}></input>
       </div>
       <div className="container"> 
+        <img src={weatherImage} alt="Wind"></img>
         <div className="Top">
           <div className='location'> 
-            <p>{ data.main ? `${data.name} ${data.sys.country}` : null}</p>
+            <p>{ data.main ? `${data.name}` : null}</p>
           </div>
-          <div className='temp'> 
-              {data.main ? <h1>{(data.main.temp - 273.15).toFixed(0)}°C</h1> : null}
-          </div>
-          <div className='description'> 
-              {data.weather ? <p>{data.weather[0].main}</p> : null}
+          <div className='middle'>
+            <div className='temp'> 
+                {data.main ? <h1>{(data.main.temp - 273.15).toFixed(0)}°C</h1> : null}
+            </div>
+            <div className='description'> 
+                {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
           </div>
         </div>
         <div className='bottom'>
           <div className='feels'>
-            {data.main ? <p>{(data.main.feels_like - 273.15).toFixed(0)}</p> : null}
+            {data.main ? <p>Feels like: <br/> {(data.main.feels_like - 273.15).toFixed(0)}°C</p> : null}
           </div>
           <div className='humidity'>
-            {data.main ? <p>{data.main.humidity}%</p> : null}
+            {data.main ? <p>Humidity: <br/> {data.main.humidity}%</p> : null}
           </div>
           <div className='wind'>
-            {data.wind ? <p>{data.wind.speed} mph</p> : null}
+            {data.wind ? <p>Wind: <br/> {data.wind.speed} mph</p> : null}
           </div>
         </div>
       </div>
